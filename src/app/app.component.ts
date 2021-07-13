a79b313eb5251212916445a527f255e855593406
RAMON COSTA MARINHO ANTUNES RAMOS <ramon.cmaramos@senacsp.edu.br>
2021-07-13 11:46:42 -0300

    construindo denovo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Login', url: '/login', icon: 'People' },
+    { title: 'Pedidos', url: '/folder/Pedidos', icon: 'bag-check' },
+    { title: 'Cliente', url: '/folder/Clientes', icon: 'body' },
+    { title: 'Devs', url: '/folder/Devs', icon: 'heart' },
+    { title: 'Devs', url: '/cadastrar', icon: 'heart' },
+  
+    
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+ 
 }
---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.router.navigate(["/login"]);
+  }
+}
