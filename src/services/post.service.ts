56c344ebe745b022959d37d213fb4b71f311e57a
RAMON COSTA MARINHO ANTUNES RAMOS <ramon.cmaramos@senacsp.edu.br>
2021-07-14 11:52:12 -0300

    adicionei api com php e services

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { stringify } from 'querystring';
 @Injectable()
 export class PostService {
 server: string= "http://localhost/pasta.php/";
   
 constructor(private http: HttpClient) { }
   dadosApi(dados:any, nomeApi: string){
-    
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type':'aplication/json'})
+    }
+    let url = this.server + nomeApi;
+    return this.http.post(url,JSON.stringify(dados),httpOptions).map(res => res)
   }
 }
